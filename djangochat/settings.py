4e63259ef32a0e01aca1b464bc406a496080a275
Arnav Mukkawar <arnavmukkawar@gmail.com>
2022-11-23 23:38:32 +0530

    Final

--- djangochat/settings.py
+++ djangochat/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'm%c30v(4=6q!_ouzf5xx9l7n89pl0#l16lo6+z*g4!49-63@t#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1' ,'chatterrrrr.herokuapp.com' ,'talkkk.herokuapp.com','chatterrplus.herokuapp.com','padhaiskool.herokuapp.com']
+ALLOWED_HOSTS = [ 'tcpchatapplication.herokuapp.com','127.0.0.1']
 
 
 # Application definition
@@ -120,7 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, "assets")
 STATIC_URL = '/static/'
 # STATICFILES_DIRS = (
 #     os.path.join(BASE_DIR, 'static'),
